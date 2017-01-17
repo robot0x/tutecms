45c97279a54ec9f1eeb679f293cb6d03de12f667
myquilt1101 <myquilt@126.com>
2017-01-17 16:46:26 +0800

    完成首页的html

--- application/block/controller/PictureNewsController.php
+++ application/block/controller/PictureNewsController.php
@@ -12,17 +12,17 @@ class PictureNewsController extends BlockController
     public function index()
     {
         // 获取内容类型
-        $contentTypeName = (string)$this->config['content_type_name'];
+        // $contentTypeName = (string)$this->config['content_type_name'];
 
-        // 获取显示的数量
-        $count = (int)$this->config['count'];
+        // // 获取显示的数量
+        // $count = (int)$this->config['count'];
 
-        // 获取新闻模型
-        $map = ['is_delete' => 0];
-        $ContentModel = new ContentModel;
-        $contentModels = $ContentModel->where($map)->limit($count)->order('id desc')->select();
-        $this->assign('contentModels', $contentModels);
-        $this->assign('menuId', ContentTypeModel::get($contentTypeName)->getData('menu_id'));
+        // // 获取新闻模型
+        // $map = ['is_delete' => 0];
+        // $ContentModel = new ContentModel;
+        // $contentModels = $ContentModel->where($map)->limit($count)->order('id desc')->select();
+        // $this->assign('contentModels', $contentModels);
+        // $this->assign('menuId', ContentTypeModel::get($contentTypeName)->getData('menu_id'));
 
         return $this->fetch();
     }
