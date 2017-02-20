c863250760afd0e46fcd24b3551b9b9f3ccf5437
mskies <3792535@qq.com>
2017-02-20 11:44:22 +0800

    增加清除缓存

--- application/admin/controller/IndexController.php
+++ application/admin/controller/IndexController.php
@@ -1,10 +1,22 @@
 <?php
 namespace app\admin\controller;
 
+use think\Cache;                    // 缓存
+
 class IndexController extends AdminController
 {
     public function indexAction()
     {
         return $this->fetch('Index/index');
+    }
+
+    public function cacheClearAction() {
+        if (Cache::clear()) {
+            return $this->success('操作成功');
+        } else {
+            return $this->error('系统发生错误');
+        }
+        
+        
     }
 }
